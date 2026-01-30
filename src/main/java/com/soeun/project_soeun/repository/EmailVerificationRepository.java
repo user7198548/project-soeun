e704042d7726a380bfc2eb563af5e074aa149f88
Soeun <user7198548@gmail.com>
2026-01-30 17:29:54 +0900

    fix: 이메일 인증 리포지토리 메서드 네이밍 수정

--- src/main/java/com/soeun/project_soeun/repository/EmailVerificationRepository.java
+++ src/main/java/com/soeun/project_soeun/repository/EmailVerificationRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
     Optional<EmailVerification> findByToken(String token);
+    boolean existsByUser_IdAndVerifiedAtIsNull(Long userId);
 }
